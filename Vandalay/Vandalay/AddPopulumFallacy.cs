6c82ae3198bffe218b376b26a7ca065c9f3718aa
nofuture-git <brian.starrett@gmail.com>
2020-11-09 10:08:58 -0600

    added the original code example

--- Vandalay/Vandalay/AddPopulumFallacy.cs
+++ Vandalay/Vandalay/AddPopulumFallacy.cs
@@ -1,38 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
-namespace CodeExampleLogic
+namespace Vandalay
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var someProp = new Proposition() {IsTrue = true};
-
-            var isAdPopulumFallacy = IsAdPopulumFallacy(someProp);
-            Console.WriteLine($"Is Ad Populum fallacy is '{isAdPopulumFallacy}'");
-        }
-
-        public static bool IsAdPopulumFallacy(Proposition someProposition)
-        {
-            var rand = new Random(DateTime.Now.Millisecond);
-
-            var bunchOfPeople = new List<Person>();
-
-            for (var i = 0; i < 100; i++)
-            {
-                var believeTrue = rand.Next() % 2 == 0;
-                bunchOfPeople.Add(new Person
-                {
-                    BelieveToBeTrue = p => believeTrue
-                });
-            }
-
-            return someProposition.BelieveItToBeSo(bunchOfPeople.ToArray()) != someProposition.IsTrue;
-        }
-    }
-
 
     /// <summary>
     /// Watch this will be flyby text!
--- Vandalay/Vandalay/Program.cs
+++ Vandalay/Vandalay/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Vandalay
 {
@@ -6,7 +7,28 @@ namespace Vandalay
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var someProp = new Proposition() { IsTrue = true };
+
+            var isAdPopulumFallacy = IsAdPopulumFallacy(someProp);
+            Console.WriteLine($"Is Ad Populum fallacy is '{isAdPopulumFallacy}'");
+        }
+
+        public static bool IsAdPopulumFallacy(Proposition someProposition)
+        {
+            var rand = new Random(DateTime.Now.Millisecond);
+
+            var bunchOfPeople = new List<Person>();
+
+            for (var i = 0; i < 100; i++)
+            {
+                var believeTrue = rand.Next() % 2 == 0;
+                bunchOfPeople.Add(new Person
+                {
+                    BelieveToBeTrue = p => believeTrue
+                });
+            }
+
+            return someProposition.BelieveItToBeSo(bunchOfPeople.ToArray()) != someProposition.IsTrue;
         }
     }
 }
