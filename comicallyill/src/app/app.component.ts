f2d69db53e86d36ddb4ec302ef20eee714bae44d
Lydia Phillips <55765132+LydiaPhillips@users.noreply.github.com>
2021-04-05 10:05:27 -0500

    comicallyill web goals and timeline

--- comicallyill/src/app/app.component.ts
+++ comicallyill/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { DashboardComponent} from './dashboard/index';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-  directives: [DashboardComponent]
 })
 export class AppComponent {
   title = 'comicallyill';
